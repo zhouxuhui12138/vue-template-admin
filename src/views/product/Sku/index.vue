f0f726daaf19c4d88870545de9b5aa349efe314e
xiaozhou <zxh04551@163.com>
2022-05-24 20:21:15 +0800

    sku end

--- src/views/product/Sku/index.vue
+++ src/views/product/Sku/index.vue
@@ -49,36 +49,38 @@
 
     <!-- 展示sku详情抽屉 -->
     <el-drawer :visible.sync="drawerVisible" size="50%" :with-header="false" @close="drawerClose">
-      <el-row :gutter="20">
-        <el-col :span="5">名称</el-col>
-        <el-col :span="16">{{ skuDetailInfo.skuName }}</el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="5">描述</el-col>
-        <el-col :span="16">{{ skuDetailInfo.skuDesc }}</el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="5">价格</el-col>
-        <el-col :span="16">{{ skuDetailInfo.price }}元</el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="5">平台属性</el-col>
-        <el-col :span="16">
-          <el-tag type="success" size="normal" v-for="item in skuDetailInfo.skuAttrValueList" :key="item.id">
-            {{ item.attrId }} - {{ item.valueId }}
-          </el-tag>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="5">商品图片</el-col>
-        <el-col :span="16">
-          <el-carousel trigger="click">
-            <el-carousel-item v-for="item in skuDetailInfo.skuImageList" :key="item.id">
-              <img :src="item.imgUrl" />
-            </el-carousel-item>
-          </el-carousel>
-        </el-col>
-      </el-row>
+      <template v-if="skuDetailInfo.skuName">
+        <el-row :gutter="20">
+          <el-col :span="5">名称</el-col>
+          <el-col :span="16">{{ skuDetailInfo.skuName }}</el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="5">描述</el-col>
+          <el-col :span="16">{{ skuDetailInfo.skuDesc }}</el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="5">价格</el-col>
+          <el-col :span="16">{{ skuDetailInfo.price }}元</el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="5">平台属性</el-col>
+          <el-col :span="16">
+            <el-tag type="success" size="normal" v-for="item in skuDetailInfo.skuAttrValueList" :key="item.id">
+              {{ item.attrId }} - {{ item.valueId }}
+            </el-tag>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="5">商品图片</el-col>
+          <el-col :span="16">
+            <el-carousel trigger="click">
+              <el-carousel-item v-for="item in skuDetailInfo.skuImageList" :key="item.id">
+                <img :src="item.imgUrl" />
+              </el-carousel-item>
+            </el-carousel>
+          </el-col>
+        </el-row>
+      </template>
     </el-drawer>
   </div>
 </template>
