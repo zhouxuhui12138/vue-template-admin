d4fb1035166858691660031c5230789141a6f2e9
xiaozhou <zxh04551@163.com>
2022-06-15 19:20:11 +0800

    完成

--- src/api/acl/user.js
+++ src/api/acl/user.js
@@ -1,10 +1,10 @@
-import request from '@/utils/request'
+import request from "@/utils/request"
 
 // 获取用户信息
 export const getInfo = () => {
   return request({
-    url: '/admin/acl/index/info',
-    method: 'get',
+    url: "/admin/acl/index/info",
+    method: "get",
   })
 }
 
@@ -12,7 +12,32 @@ export const getInfo = () => {
 export const getUserList = (page, limit, params) => {
   return request({
     url: `/admin/acl/user/${page}/${limit}`,
-    method: 'get',
-    params
+    method: "get",
+    params,
+  })
+}
+
+// 更新用户信息
+export const updateUser = data => {
+  return request({
+    url: `/admin/acl/user/update`,
+    method: "put",
+    data,
+  })
+}
+
+// 删除用户
+export const deleteUser = id => {
+  return request({
+    url: `/admin/acl/user/remove/${id}`,
+    method: "delete",
+  })
+}
+
+// 获取用户的所有角色
+export const getUserRoles = id => {
+  return request({
+    url: `/admin/acl/user/toAssign/${id}`,
+    method: "get",
   })
 }
--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -5,8 +5,8 @@
       <el-form-item>
         <el-input v-model="searchKey" placeholder="用户名"></el-input>
       </el-form-item>
-      <el-button type="primary" icon="el-icon-search">查询</el-button>
-      <el-button plain>清空</el-button>
+      <el-button type="primary" icon="el-icon-search" @click="getUserList(page, pageSize)">查询</el-button>
+      <el-button plain @click="clearSearch">清空</el-button>
     </el-form>
     <!-- 添加或删除 -->
     <div class="add-delete">
@@ -14,7 +14,7 @@
       <el-button type="danger" plain>批量删除</el-button>
     </div>
     <!-- table -->
-    <el-table :data="userList" style="width: 100%" border>
+    <el-table :data="userList" style="width: 100%" border v-loading="loading">
       <el-table-column type="selection" width="55"> </el-table-column>
       <el-table-column type="index" label="序号" width="55"></el-table-column>
       <el-table-column prop="username" label="用户名"></el-table-column>
@@ -23,11 +23,16 @@
       <el-table-column prop="gmtCreate" label="创建时间"></el-table-column>
       <el-table-column prop="gmtModified" label="更新事件"></el-table-column>
       <el-table-column label="操作" align="center">
-        <el-button type="info" icon="el-icon-user" size="mini"></el-button>
-        <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-        <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+        <template slot-scope="{ row }">
+          <el-button type="info" @click="setRole(row.id)" icon="el-icon-user" size="mini"></el-button>
+          <el-button type="primary" @click="editUser(row)" icon="el-icon-edit" size="mini"></el-button>
+          <el-popconfirm placement="top" style="margin-left: 10px" title="确定删除吗?" @onConfirm="deleteUser(row.id)">
+            <el-button type="danger" slot="reference" icon="el-icon-delete" size="mini"></el-button>
+          </el-popconfirm>
+        </template>
       </el-table-column>
     </el-table>
+
     <!-- 分页 -->
     <el-pagination
       @size-change="handleSizeChange"
@@ -39,11 +44,29 @@
       :total="total"
     >
     </el-pagination>
+
+    <!-- 修改用户 -->
+    <el-dialog title="修改用户" :visible.sync="editUserDialog" width="50%" @close="closeEditUser">
+      <el-form :model="editUserForm" label-width="80px" ref="editUserRef" :rules="rules">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="editUserForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="用户昵称">
+          <el-input v-model="editUserForm.nickName"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer">
+        <el-button @click="editUserDialog = false">取消</el-button>
+        <el-button type="primary" @click="submitEditUser">确定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getInfo, getUserList } from "@/api/acl/user"
+import { getUserList, updateUser, deleteUser, getUserRoles } from "@/api/acl/user"
+import { Message } from "element-ui"
 
 export default {
   data() {
@@ -52,25 +75,87 @@ export default {
       total: 0,
       page: 1,
       pageSize: 5,
-      searchKey: ''
+      searchKey: "",
+      loading: true,
+      editUserDialog: false,
+      editUserForm: {},
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 10, message: "长度在 5 到 10 个字符", trigger: "blur" },
+        ],
+      },
     }
   },
   created() {
     this.getUserList(this.page, this.pageSize)
   },
   methods: {
+    // 获取用户数据
     async getUserList(page, pageSize) {
+      this.loading = true
       const { data: res } = await getUserList(page, pageSize, { username: this.searchKey })
-      this.userList = res.items
-      this.total = res.total
+      this.userList = res.items.filter(item => item.username !== "admin")
+      this.total = res.total - 1
+      this.loading = false
     },
+    // 改变pageSize
     handleSizeChange(pageSize) {
       this.pageSize = pageSize
       this.getUserList(this.page, this.pageSize)
     },
+    // 改变page
     handleCurrentChange(page) {
       this.page = page
       this.getUserList(this.page, this.pageSize)
+    },
+    // 取消搜索
+    clearSearch() {
+      this.searchKey = ""
+      this.getUserList(this.page, this.pageSize)
+    },
+    // 弹出修改用户
+    editUser(row) {
+      // 深拷贝 阻止浅拷贝引用错误
+      const obj = JSON.stringify(row)
+      this.editUserForm = JSON.parse(obj)
+      this.editUserDialog = true
+    },
+    // 提交修改用户
+    submitEditUser() {
+      this.$refs["editUserRef"].validate(async valid => {
+        if (!valid) return
+        try {
+          await updateUser(this.editUserForm)
+          this.getUserList(this.page, this.pageSize)
+          Message({ type: "success", message: "修改成功" })
+        } catch (error) {
+          Message({ type: "error", message: error })
+        }
+
+        this.editUserDialog = false
+      })
+    },
+    // 关闭修改用户
+    closeEditUser() {
+      this.editUserForm = {}
+    },
+    // 删除用户
+    async deleteUser(id) {
+      try {
+        await deleteUser(id)
+        this.getUserList(this.page, this.pageSize)
+        Message({ type: "success", message: "删除成功" })
+      } catch (error) {
+        Message({ type: "error", message: error })
+      }
+    },
+    // 展示和修改用户的角色
+    async setRole(id) {
+      try {
+        const res = await getUserRoles(id)
+        console.log(res)
+      } catch (error) {}
     },
   },
 }
